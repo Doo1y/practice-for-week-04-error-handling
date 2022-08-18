7e1bc860b2b617ca37cc18f0b48177f0622aaad9
Samuel Park <s.park111497@gmail.com>
2022-08-18 12:18:02 -0700

    finished exercise

--- error-handling.js
+++ error-handling.js
@@ -7,13 +7,37 @@ function sum(array) {
   return sum;
 }
 
-let res = sum(null);
-console.log(res);
+try {
+  let res = sum(null);
+  console.log(res);
+} catch (error) {
+  if (error instanceof TypeError) {
+
+    console.error(`${error.name} : ${error.message}`, error.getFileName);
+  } else if (error instanceof RangeError) {
+    console.error("RangeError");
+  } else if (error instanceof SyntaxError) {
+    console.error("SyntaxError");
+  }
+} finally {
+  console.log("No other errors");
+}
+
 
 // 2.
-// tests
-sayName("Alex");
-sayName(1);
+sayName = (name) => {
+  if (typeof name !== "string") throw new TypeError(
+    "Invalid name! Must be a string!"
+  );
+  else console.log(name);
+}
+
+try { 
+  sayName("Alex"); 
+  sayName(1); 
+} catch (error) {
+  console.log(error.name + " : " + error.message);
+}
 // Your code here
 
 // 3.
@@ -23,4 +47,10 @@ function greet(greeting) {
   }
 
   console.log(greeting);
+}
+
+try {
+  greet()
+} catch (error) {
+  console.log("Hello World!")
 }
